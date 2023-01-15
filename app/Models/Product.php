0859919b182f15ab04124b5fe19871cf685d1249
marmol89 <josito_marmol@hotmail.com>
2023-01-15 19:00:34 +0100

    Paso 11 [S-1 Factories y Seeders]

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    const BORRADOR = 1;
+    const PUBLICADO = 2;
 
     protected $fillable = ['name', 'slug', 'description', 'price', 'subcategory_id', 'brand_id', 'quantity'];
     //protected $guarded = ['id', 'created_at', 'updated_at'];
