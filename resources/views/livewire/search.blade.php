df0aaf181d56c711689ddd12c2348253edf4321f
josit <josito_marmol@hotmail.com>
2023-01-16 16:22:06 +0100

    Paso 17 v3 [S-1 Layout y barra de menu]

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,7 +1,3 @@
 <div class="flex-1">
-<<<<<<< HEAD
     <x-jet-input type="text" class="flex w-full" placeholder="¿Estás buscando algún producto?"></x-jet-input>
-=======
-    <x-jet-input type="text"></x-jet-input>
->>>>>>> origin/master
 </div>
