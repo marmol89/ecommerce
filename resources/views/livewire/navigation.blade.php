2604ee76d947d37f64588c3a1034a93df9805c03
josit <josito_marmol@hotmail.com>
2023-01-17 18:57:00 +0100

    Paso 21 [S-1 Despliegue de Categorías y Subcategorías]

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,9 @@
-<header class="bg-trueGray-700">
+<style>
+    #navigation-menu{
+        height: calc(100vh - 4rem);
+    }
+</style>
+<header class="bg-trueGray-700 sticky top-0">
     <div class="container-menu flex items-center h-16">
         <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -63,4 +68,26 @@
         </div>
         @livewire('dropdown-cart')
     </div>
+    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+        <div class="container-menu h-full">
+            <div class="grid grid-cols-4 h-full relative">
+                <ul class="bg-white">
+                    @foreach($categories as $category)
+                        <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                                <span class="flex justify-center w-9">
+                                    {!! $category->icon !!}
+                                </span>
+                                {{ $category->name }}
+                            </a>
+                            <div class="bg-red-500 absolute w-3/4 h-full top-0 right-0">
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="col-span-3 bg-gray-100">
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
