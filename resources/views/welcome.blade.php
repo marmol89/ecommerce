4d60194975d800c2c0ad26b16c95edd7db290326
josit <josito_marmol@hotmail.com>
2023-01-18 18:00:38 +0100

    Paso 33 [S-2 Slider en página principal]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,18 @@
 <x-app-layout>
     <div class="container-menu py-8">
-        <section>
+        @foreach($categories as $category)
+        <section class="mb-6">
             <h1 class="text-lg uppercase font-semibold text-gray-700">
-                {{ $categories->first()->name }}
+                {{ $category->name }}
             </h1>
-            @livewire('category-products', ['category' => $categories->first()])
+            @livewire('category-products', ['category' => $category])
         </section>
+        @endforeach
     </div>
     @push('scripts')
     <script>
-        Livewire.on('glider', function () {
-            new Glider(document.querySelector('.glider'), {
+        Livewire.on('glider', function (id) {
+            new Glider(document.querySelector('.glider-' + id), {
                 slidesToShow: 5.5,
                 slidesToScroll: 5,
                 draggable: true,
