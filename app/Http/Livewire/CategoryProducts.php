68fa05e572a73d2aef2ba231d22204fc39474036
josit <josito_marmol@hotmail.com>
2023-01-18 17:52:37 +0100

    Paso 31 [S-2 Slider en página principal]

--- app/Http/Livewire/CategoryProducts.php
+++ app/Http/Livewire/CategoryProducts.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class CategoryProducts extends Component
 {
+    public $category;
     public function render()
     {
         return view('livewire.category-products');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', WelcomeController::class);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
