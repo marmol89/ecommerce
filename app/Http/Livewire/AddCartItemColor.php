b57002faeadc6e37f6680a479ab0d8800d5a02f7
josit <josito_marmol@hotmail.com>
2023-01-19 16:35:29 +0100

    Paso 51 revisar [S-2 Productos 1 parte]

--- app/Http/Livewire/AddCartItemColor.php
+++ app/Http/Livewire/AddCartItemColor.php
@@ -7,26 +7,28 @@ use Livewire\Component;
 class AddCartItemColor extends Component
 {
     public $product;
-
     public $colors;
-
     public $qty = 1;
-
     public $quantity = 0;
-
     public $color_id = '';
-
-    public function updatedColorId($value)
-    {
-        $this->quantity = $this->product->colors->find($value)->pivot->quantity;
-    }
-
     public function mount()
     {
         $this->colors = $this->product->colors;
     }
+    public function decrement()
+    {
+        $this->qty--;
+    }
+    public function increment()
+    {
+        $this->qty++;
+    }
     public function render()
     {
         return view('livewire.add-cart-item-color');
+    }
+    public function updatedColorId($value)
+    {
+        $this->quantity = $this->product->colors->find($value)->pivot->quantity;
     }
 }
