8e6a25c30b1a12867c2c3720b6004019ea13aefd
marmol89 <josito_marmol@hotmail.com>
2023-02-03 19:52:42 +0100

    Paso 80 [S-3 Carrito de Compra]

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -71,4 +71,19 @@
             </div>
         @endif
     </section>
+    @if(Cart::count())
+        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mt-4">
+            <div class="flex justify-between items-center">
+                <div class="text-gray-700">
+                    <span class="font-bold text-lg">Total:</span>
+                    {{ Cart::subtotal() }} &euro;
+                </div>
+                <div>
+                    <x-button-link>
+                        Continuar
+                    </x-button-link>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
