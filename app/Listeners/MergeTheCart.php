1457fa179076bd0f50335aeb4518b1f4654ef4de
marmol89 <josito_marmol@hotmail.com>
2023-02-03 20:10:43 +0100

    Paso 84 [S-3 Crear pedidos 1 parte]

--- app/Listeners/MergeTheCart.php
+++ app/Listeners/MergeTheCart.php
@@ -2,7 +2,8 @@
 
 namespace App\Listeners;
 
-use App\Events\Login;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -26,6 +27,6 @@ class MergeTheCart
      */
     public function handle(Login $event)
     {
-        //
+        Cart::merge(auth()->user()->id);
     }
 }
--- app/Listeners/MergeTheCartLogout.php
+++ app/Listeners/MergeTheCartLogout.php
@@ -2,9 +2,10 @@
 
 namespace App\Listeners;
 
-use App\Events\Logout;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Events\Logout;
 
 class MergeTheCartLogout
 {
@@ -26,6 +27,7 @@ class MergeTheCartLogout
      */
     public function handle(Logout $event)
     {
-        //
+        Cart::erase(auth()->user()->id);
+        Cart::store(auth()->user()->id);
     }
 }
