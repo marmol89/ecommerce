79b597511332b957b39dde53109a458e2639a359
marmol89 <josito_marmol@hotmail.com>
2023-02-06 19:41:09 +0100

    Paso 110 [S-4 Gestionar Pedidos]

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,13 +10,7 @@ class OrderController extends Controller
 
     public function show(Order $order)
     {
-        return view('orders.show', compact('order'));
-    }
-
-
-    public function payment(Order $order)
-    {
         $items = json_decode($order->content);
-        return view('orders.payment', compact('order' , 'items'));
+        return view('orders.show', compact('order' , 'items'));
     }
 }
