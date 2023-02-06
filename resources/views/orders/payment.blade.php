96502ca0afa07e59a02ea17b05b90e7f2e79773b
marmol89 <josito_marmol@hotmail.com>
2023-02-06 19:01:49 +0100

    Paso 108 [S-4 Paypal]

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
-    <div class="grid grid-cols-5 gap-6 container-menu py-8">
+    <div class="grid grid-cols-5 gap-6 container-menu py8">
         <div class="col-span-3">
             <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
                 <p class="text-gray-700 uppercase"><span class="font-semibold">Número de Orden:</span> {{ $order->id }}</p>
             </div>
+
             <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
                 <div class="grid grid-cols-2 gap-6">
                     <div>
@@ -18,15 +19,18 @@
                             <p>{{ $order->department->name }} - {{ $order->city->name }} - {{ $order->district->name }}</p>
                         @endif
                     </div>
-                </div>
-                <div>
-                    <p class="text-lg font-semibold uppercase">Datos de contacto</p>
-                    <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
-                    <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
+
+                    <div>
+                        <p class="text-lg font-semibold uppercase">Datos de contacto</p>
+                        <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
+                        <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
+                    </div>
                 </div>
             </div>
+
             <div class="bg-white rounded-lg shadow-lg p-6 text-gray-700">
                 <p class="text-xl font-semibold mb-4">Resumen</p>
+
                 <table class="table-auto w-full">
                     <thead>
                     <tr>
@@ -36,6 +40,7 @@
                         <th>Total</th>
                     </tr>
                     </thead>
+
                     <tbody class="divide-y divide-gray-200">
                     @foreach($items as $item)
                         <tr>
@@ -43,8 +48,10 @@
                                 <div class="flex">
                                     <img class="h-15 w-20 object-cover mr-4"
                                          src="{{ $item->options->image }}" alt="">
+
                                     <article>
                                         <h1 class="font-bold">{{ $item->name }}</h1>
+
                                         <div class="flex text-xs">
                                             @isset($item->options->color)
                                                 Color: {{ __(ucfirst($item->options->color)) }}
@@ -56,12 +63,15 @@
                                     </article>
                                 </div>
                             </td>
+
                             <td class="text-center">
                                 {{ $item->price }} &euro;
                             </td>
+
                             <td class="text-center">
                                 {{ $item->qty }}
                             </td>
+
                             <td class="text-center">
                                 {{ $item->price * $item->qty }} &euro;
                             </td>
@@ -71,24 +81,50 @@
                 </table>
             </div>
         </div>
-            <div class="col-span-2">
-                <div class="bg-white rounded-lg shadow-lg p-6">
-                    <div class="flex justify-between items-center">
-                        <img class="h-8" src="{{ asset('img/MC_VI_DI_2-1.jpg') }}" alt="">
-                        <div class="text-gray-700">
-                            <p class="text-sm font-semibold">
-                                Subtotal: {{ $order->total - $order->shipping_cost }} &euro;
-                            </p>
-                            <p class="text-sm font-semibold">
-                                Envío: {{ $order->shipping_cost }} &euro;
-                            </p>
-                            <p class="text-lg font-semibold uppercase">
-                                Pago: {{ $order->total }} &euro;
-                            </p>
-                        </div>
+
+        <div class="col-span-2">
+            <div class="bg-white rounded-lg shadow-lg px-6 pt-6">
+                <div class="flex justify-between items-center mb-4">
+                    <img class="h-8" src="{{ asset('img/MC_VI_DI_2-1.jpg') }}" alt="">
+
+                    <div class="text-gray-700">
+                        <p class="text-sm font-semibold">
+                            Subtotal: {{ $order->total - $order->shipping_cost }} &euro;
+                        </p>
+                        <p class="text-sm font-semibold">
+                            Envío: {{ $order->shipping_cost }} &euro;
+                        </p>
+                        <p class="text-lg font-semibold uppercase">
+                            Pago: {{ $order->total }} &euro;
+                        </p>
                     </div>
                 </div>
+
+                <div id="paypal-button-container"></div>
             </div>
         </div>
     </div>
+
+    <script src="https://www.paypal.com/sdk/js?client-id={{ config('services.paypal.client_id') }}&currency=EUR"></script>
+
+    <script>
+        paypal.Buttons({
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: '77.44'
+                        }
+                    }]
+                });
+            },
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(orderData) {
+                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                    var transaction = orderData.purchase_units[0].payments.captures[0];
+                    alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+                });
+            }
+        }).render('#paypal-button-container');
+    </script>
 </x-app-layout>
