d2629100e9f39975783b7b2d7201183bf1084670
marmol89 <josito_marmol@hotmail.com>
2023-02-07 19:22:55 +0100

    Paso 114 [S-4 Gestionar Pedidos]

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,7 +9,8 @@ class OrderController extends Controller
 {
     public function index()
     {
-        return view('orders.index');
+        $orders = Order::where('user_id', auth()->user()->id)->get();
+        return view('orders.index', compact('orders'));
     }
 
     public function show(Order $order)
