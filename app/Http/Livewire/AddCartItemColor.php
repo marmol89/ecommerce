bab773f9f79e6d4a88a83d3885e6f8b68533fe6e
marmol89 <josito_marmol@hotmail.com>
2023-02-07 19:35:06 +0100

    Paso 117 [S-4 Gestionar Pedidos]

--- app/Http/Livewire/AddCartItemColor.php
+++ app/Http/Livewire/AddCartItemColor.php
@@ -55,5 +55,6 @@ class AddCartItemColor extends Component
         $color = $this->product->colors->find($value);
         $this->quantity = qty_available($this->product->id, $color->id);
         $this->options['color'] = $color->name;
+        $this->options['color_id'] = $color->id;
     }
 }
