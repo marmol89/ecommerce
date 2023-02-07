60ca0fa7125a5ab19f896607b664e3ca58ab9c56
marmol89 <josito_marmol@hotmail.com>
2023-02-07 21:07:24 +0100

    Paso 118 [S-4 Gestionar Pedidos]

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -59,6 +59,11 @@ class CreateOrder extends Component
         }
 
         $order->save();
+
+        foreach (Cart::content() as $item) {
+            discount($item);
+        }
+
         Cart::destroy();
         return redirect()->route('orders.payment', $order);
     }
