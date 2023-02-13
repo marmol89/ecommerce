22b94c96eeaf7e513c5e884406d3b7ef7d3cb715
marmol89 <josito_marmol@hotmail.com>
2023-02-13 17:12:29 +0100

    Paso 167 [S-5 Gestión de categorías]

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Livewire\Admin\CreateProduct;
 use App\Http\Livewire\Admin\EditProduct;
+use App\Http\Livewire\Admin\ShowCategory;
 use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,4 @@ Route::get('products/create', CreateProduct::class)->name('admin.products.create
 Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
 Route::post('product/{product}/files', [ProductController::class, 'files'])->name('admin.products.files');
 Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories.index');
+Route::get('categories/{category}', ShowCategory::class)->name('admin.categories.show');
