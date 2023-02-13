ad56974c56eef49442871325bce8a1f67df9f703
marmol89 <josito_marmol@hotmail.com>
2023-02-13 14:39:24 +0100

    Paso 157 [S-4 Gestión de imágenes]

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Livewire\Admin\CreateProduct;
 use App\Http\Livewire\Admin\EditProduct;
@@ -11,3 +12,4 @@ Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('products/create', CreateProduct::class)->name('admin.products.create');
 Route::get('products/{product}/edit', EditProduct::class)->name('admin.products.edit');
 Route::post('product/{product}/files', [ProductController::class, 'files'])->name('admin.products.files');
+Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories.index');
