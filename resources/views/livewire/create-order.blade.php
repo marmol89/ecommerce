153aa31d5452375c9d46c2fb5db880efa5ffbc07
marmol89 <josito_marmol@hotmail.com>
2023-02-13 22:49:21 +0100

    Paso 189 [S-5 Diseño responsivo]

--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -1,5 +1,5 @@
-<div class="container-menu py-8 grid grid-cols-5 gap-6">
-    <div class="col-span-3">
+<div class="container-menu py-8 grid lg:grid-cols-2 xl:grid-cols-5 gap-6">
+    <div class="order-2 lg:order-1 lg:col-span-1 xl:col-span-3">
         <div class="bg-white rounded-lg shadow p-6">
             <div class="mb-4">
                 <x-jet-label value="Nombre de contacto" />
@@ -93,7 +93,7 @@
                 <a href="" class="font-semibold text-orange-500">Políticas y privacidad</a></p>
         </div>
     </div>
-    <div class="col-span-2">
+    <div class="order-1 lg:order-2 lg:col-span-1 xl:col-span-2">
         <div class="bg-white rounded-lg shadow p-6"><ul>
                 @forelse(Cart::content() as $item)
                     <li class="flex p-2 border-b border-gray-200">
