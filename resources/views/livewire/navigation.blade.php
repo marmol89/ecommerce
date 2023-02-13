693d0f18bef75888cbde80c62c18ca2eb764fbb0
marmol89 <josito_marmol@hotmail.com>
2023-02-13 20:59:01 +0100

    Paso 181 [S-5 Laravel-permission]

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -40,11 +40,11 @@
                         <x-jet-dropdown-link href="{{ route('orders.index') }}">
                             {{ __('My Orders') }}
                         </x-jet-dropdown-link>
-
+                        @role('admin')
                         <x-jet-dropdown-link href="{{ route('admin.index') }}">
                             {{ __('Admin') }}
                         </x-jet-dropdown-link>
-
+                        @endrole
                         <div class="border-t border-gray-100"></div>
 
                         <!-- Authentication -->
