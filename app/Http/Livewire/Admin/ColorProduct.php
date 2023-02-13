39e92d3abb8dc6400f5f16dd12f5e275b608ae36
marmol89 <josito_marmol@hotmail.com>
2023-02-13 12:25:01 +0100

    Paso 136 [S-4 Componente color-product]

--- app/Http/Livewire/Admin/ColorProduct.php
+++ app/Http/Livewire/Admin/ColorProduct.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Color;
 use Livewire\Component;
 
 class ColorProduct extends Component
 {
+    public $product, $colors;
+    public $color_id, $quantity;
+
+    protected $rules = [
+        'color_id' => 'required',
+        'quantity' => 'required|numeric'
+    ];
+
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
+    public function save(){
+        $this->validate();
+
+        $this->product->colors()->attach([
+            $this->color_id => [
+                'quantity' => $this->quantity
+            ]
+        ]);
+
+        $this->reset(['color_id', 'quantity']);
+        $this->emit('saved');
+    }
+
     public function render()
     {
         return view('livewire.admin.color-product');
