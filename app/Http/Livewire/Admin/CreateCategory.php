cfba507c718714f7f6f1aa3f9fa101289635c81f
marmol89 <josito_marmol@hotmail.com>
2023-02-13 16:37:57 +0100

    Paso 159 [S-5 Gestión de categorías]

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -2,29 +2,59 @@
 
 namespace App\Http\Livewire\Admin;
 
+use WithFileUploads;
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class CreateCategory extends Component
 {
-
     public $brands;
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
+
     public function mount()
     {
         $this->getBrands();
     }
+
     public function getBrands()
     {
         $this->brands = Brand::all();
     }
 
+    public function save()
+    {
+        $this->validate();
+    }
 
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
 
     public function render()
     {
         return view('livewire.admin.create-category');
-    }
-    public function save()
-    {
     }
 }
