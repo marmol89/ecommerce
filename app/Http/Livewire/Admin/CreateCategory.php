cd5ef0350ec787c375692332fbb17a513a5cdcfd
marmol89 <josito_marmol@hotmail.com>
2023-02-13 16:59:17 +0100

    Paso 164 [S-5 Gestión de categorías]

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -99,6 +99,8 @@ class CreateCategory extends Component
 
     public function edit(Category $category)
     {
+        $this->image = rand();
+        $this->reset(['editImage']);
         $this->category = $category;
 
         $this->editForm['open'] = true;
