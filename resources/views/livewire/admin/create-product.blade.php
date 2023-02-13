dfe7291b17627961cbd74897ecf948fc693081b8
marmol89 <josito_marmol@hotmail.com>
2023-02-13 11:58:51 +0100

    Paso 127 [S-4 Creación de Productos]

--- resources/views/livewire/admin/create-product.blade.php
+++ resources/views/livewire/admin/create-product.blade.php
@@ -60,4 +60,14 @@
             </textarea>
         </div>
     </div>
+
+    <div class="mb-4">
+        <x-jet-label value="Marca" />
+        <select class="form-control w-full" wire:model="brand_id">
+            <option value="" selected disabled>Seleccione una marca</option>
+            @foreach ($brands as $brand)
+                <option value="{{$brand->id}}">{{$brand->name}}</option>
+            @endforeach
+        </select>
+    </div>
 </div>
