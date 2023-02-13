7fa1e0f795e5eba634a69288c1ab1b14bd84e484
marmol89 <josito_marmol@hotmail.com>
2023-02-13 12:16:44 +0100

    Paso 134 [S-4 Creación de Productos]

--- app/Http/Livewire/Admin/EditProduct.php
+++ app/Http/Livewire/Admin/EditProduct.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\Subcategory;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class EditProduct extends Component
@@ -22,6 +23,7 @@ class EditProduct extends Component
         'product.description' => 'required',
         'product.brand_id' => 'required',
         'product.price' => 'required',
+        'product.quantity' => 'numeric',
     ];
 
     public function mount(Product $product)
@@ -52,6 +54,25 @@ class EditProduct extends Component
 
         $this->product->subcategory_id = '';
         $this->product->brand_id = '';
+    }
+
+    public function updatedProductName($value){
+        $this->product->slug = Str::slug($value);
+    }
+
+    public function save()
+    {
+        $this->rules['product.slug'] = 'required|unique:products,slug,' . $this->product->id;
+
+        if ($this->product->subcategory_id) {
+            if (!$this->subcategory->color && !$this->subcategory->size) {
+                $this->rules['product.quantity'] = 'required|numeric';
+            }
+        }
+
+        $this->validate();
+
+        $this->product->save();
     }
 
     public function render()
