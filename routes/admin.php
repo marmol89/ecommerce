89b1aa58d6a6fa98869253282e053382aa1bb1bb
marmol89 <josito_marmol@hotmail.com>
2023-02-13 11:52:37 +0100

    Paso 124 [S-4 Creación de Productos]

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@
 use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', ShowProducts::class)->name('admin.index');
 
+Route::get('products/create', function () {})->name('admin.products.create');
 Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
