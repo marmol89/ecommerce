bb8d5b48b1a4caa7910bdd3dcf426952f01b1ad4
marmol89 <josito_marmol@hotmail.com>
2023-02-13 20:09:44 +0100

    Paso 177 [S-5 Gestión de departamentos, ciudades y distritos]

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\Admin\CreateProduct;
 use App\Http\Livewire\Admin\DepartmentComponent;
 use App\Http\Livewire\Admin\EditProduct;
 use App\Http\Livewire\Admin\ShowCategory;
+use App\Http\Livewire\Admin\ShowCity;
 use App\Http\Livewire\Admin\ShowDepartment;
 use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,4 @@ Route::get('orders', [OrderController::class, 'index'])->name('admin.orders.inde
 Route::get('orders/{order}', [OrderController::class, 'show'])->name('admin.orders.show');
 Route::get('departments', DepartmentComponent::class)->name('admin.departments.index');
 Route::get('departments/{department}', ShowDepartment::class)->name('admin.departments.show');
+Route::get('cities/{city}', ShowCity::class)->name('admin.cities.show');
