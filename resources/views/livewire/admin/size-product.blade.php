e35bb55535be721c0a38e1fb4d7bc1ed724a6a5d
marmol89 <josito_marmol@hotmail.com>
2023-02-13 12:58:33 +0100

    Paso 145 [S-4 Componente size-product]

--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -36,6 +36,8 @@
                         </x-jet-danger-button>
                     </div>
                 </div>
+
+                @livewire('admin.color-size', ['size' => $size], key('color-size-' . $size->id))
             </li>
         @endforeach
     </ul>
